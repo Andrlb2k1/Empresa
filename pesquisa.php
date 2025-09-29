2f5daac8efaf9824dcc550f4487b8c8fe387a80b
unknown <andre.lb865854@gmail.com>
2025-09-29 11:52:10 -0300

    search screen

--- pesquisa.php
+++ pesquisa.php
@@ -8,14 +8,57 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="css/bootstrap.min.css">
 
-    <title>Cadastro</title>
+    <title>Pesquisar</title>
   </head>
   <body>
-    <div class="container" method="POST">
+
+    <?php
+        $pesquisa = $_POST['busca'] ?? '';
+        
+        include "conexao.php";
+
+        $sql = "SELECT * FROM pessoas WHERE nome LIKE '%$pesquisa%'";
+
+        $dados = mysqli_query($conn, $sql);
+
+        while($linha = mysqli_fetch_assoc($dados)) {
+            foreach($linha as $key => $value) {
+                echo "$key: $value<br>";
+            }
+            echo "<hr>";
+        }
+    ?>
+    
+    <div class="container">
         <div class="row">
             <div class="col">
-                <h1>Cadastro</h1>
-                
+                <h1>Pesquisar</h1>
+                <nav class="navbar navbar-light bg-light">
+                    <form class="d-flex" role="search" action="pesquisa.php" method="POST">
+                        <input class="form-control me-2" type="search" placeholder="Nome" aria-label="Search" name="busca"/>
+                        <button class="btn btn-outline-success" type="submit">Pesquisar</button>
+                    </form>
+                </nav>
+                <table class="table table-hover">
+                    <thead>
+                        <tr>
+                            <th scope="col">Nome</th>
+                            <th scope="col">Endereço</th>
+                            <th scope="col">Telefone</th>
+                            <th scope="col">Email</th>
+                            <th scope="col">Data de Nascimento</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">Nome</th>
+                            <td>Rua x</td>
+                            <td>48 998087155</td>
+                            <td>fulano@gmail.com</td>
+                            <td>12/11/2001</td>
+                        </tr>
+                    </tbody>
+                </table>
                 <br>
                 <a href="index.php" class="btn btn-info">Voltar ao início</a>
             </div>
