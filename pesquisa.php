6955af6624322af152edb07b9b5aea3833ffe039
unknown <andre.lb865854@gmail.com>
2025-09-29 11:09:43 -0300

    new index

--- pesquisa.php
+++ pesquisa.php
@@ -8,20 +8,16 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="css/bootstrap.min.css">
 
-    <title>Empresa</title>
+    <title>Cadastro</title>
   </head>
   <body>
     <div class="container" method="POST">
         <div class="row">
             <div class="col">
-                <div class="jumbotron">
-                    <h1 class="display-4">Cadastro Web</h1>
-                    <p class="lead">Este é um sistema simplificado de cadastros. Base de estudos para a criação de sistemas Web com PHP e MySQL.</p>
-                    <hr class="my-4">
-                    <p>Acesse as funções.</p>
-                    <a class="btn btn-primary btn-lg" href="cadastro.php" role="button">Cadastro</a>
-                    <a class="btn btn-primary btn-lg" href="pesquisa.php" role="button">Pesquisar</a>
-                </div>
+                <h1>Cadastro</h1>
+                
+                <br>
+                <a href="index.php" class="btn btn-info">Voltar ao início</a>
             </div>
         </div>
     </div>
